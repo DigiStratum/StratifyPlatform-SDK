e2f43feaf8bcbc8f1ea9676025ec4ccfb59fcd6c
Sean Kelly <keenskelly@gmail.com>
2021-05-13 22:29:42 -0700

    Refactor to HttpRequestIfc from HttpRequest

--- lib/Module/endpoint.go
+++ lib/Module/endpoint.go
@@ -36,41 +36,41 @@ type ConfigurableEndpointIfc interface {
 
 // Optional: Endpoint public interface: ANY METHOD request handling
 type AnyEndpointIfc interface {
-	HandleAny(request *rest.HttpRequest) *rest.HttpResponse
+	HandleAny(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: GET request handling
 type GetEndpointIfc interface {
-	HandleGet(request *rest.HttpRequest) *rest.HttpResponse
+	HandleGet(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: POST request handling
 type PostEndpointIfc interface {
-	HandlePost(request *rest.HttpRequest) *rest.HttpResponse
+	HandlePost(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: PUT request handling
 type PutEndpointIfc interface {
-	HandlePut(request *rest.HttpRequest) *rest.HttpResponse
+	HandlePut(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: OPTIONS request handling
 type OptionsEndpointIfc interface {
-	HandleOptions(request *rest.HttpRequest) *rest.HttpResponse
+	HandleOptions(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: HEAD request handling
 type HeadEndpointIfc interface {
-	HandleHead(request *rest.HttpRequest) *rest.HttpResponse
+	HandleHead(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: DELETE request handling
 type DeleteEndpointIfc interface {
-	HandleDelete(request *rest.HttpRequest) *rest.HttpResponse
+	HandleDelete(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
 // Optional: Endpoint public interface: PATCH request handling
 type PatchEndpointIfc interface {
-	HandlePatch(request *rest.HttpRequest) *rest.HttpResponse
+	HandlePatch(request rest.HttpRequestIfc) *rest.HttpResponse
 }
 
