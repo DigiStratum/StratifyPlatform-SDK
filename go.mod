7bcc7ff94c61b63872f87218f56ae9da89edfcc7
Sean Kelly <keenskelly@gmail.com>
2022-12-10 15:57:45 -0800

    WIP - Expand support for Auth layer
    This auth security layer is essential to Module development so that creators can secure their module and manage authentication and authorization in a flexible way.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/DigiStratum/GoStratifyPlatform-SDK
+
+go 1.17
+
+require github.com/DigiStratum/GoLib v0.0.0-20221210223712-45b793a24420
---
+++ lib/Auth/authorizationgrants.go
@@ -0,0 +1,54 @@
+package auth
+
+/*
+Hold a list of Authorization Grants
+
+This is meant to reflect a set of Authorizations within a given context (Scope/Realm/App), etc for a
+givn user Identity. That said, the model is generalized enough that could be used in other ways as
+well such as setting up Authorization packages that can be granted/revoked in bulk for administrative
+purposes, etc.
+
+Each Authorization Grant is identified by a unique string (key) and bears an optional value for a
+Metadata string; both the Authorization Grant name and the metadata are context-specific. That is to
+say that there could be a valid authorization grant named "fulladmin" for two entirely different
+contexts. It is the consumer's responsibility to organize the AuthorizationGrants collections by
+context/identity as this could take any number of forms and is outside the scope of this interface.
+
+Our underlying HashMap data structure is organized as: [GrantName] -> [Context-Specific-Metadata]
+
+*/
+
+import (
+	hash "github.com/DigiStratum/GoLib/Data/hashmap"
+)
+
+type ScopedAuthorizationGrantsIfc interface {
+	// Embed HashMapIfc to inherit all of that...
+	hash.HashMapIfc
+}
+
+type scopedAuthorizationGrants struct {
+	// Embed HashMap struct to inherit all of that...
+	*hash.HashMap
+	scopeName	string
+}
+
+// -------------------------------------------------------------------------------------------------
+// Factory Functions
+// -------------------------------------------------------------------------------------------------
+
+func NewScopedAuthorizationGrants(scopeName string) *scopedAuthorizationGrants {
+	return &scopedAuthorizationGrants{
+		HashMap:	hash.NewHashMap(),
+		scopeName:	scopeName,
+	}
+}
+
+// -------------------------------------------------------------------------------------------------
+// ScopedAuthorizationGrantsIfc Implementation
+// -------------------------------------------------------------------------------------------------
+
+func (r *scopedAuthorizationGrants) GetScopeName() string {
+	return r.scopeName
+}
+
