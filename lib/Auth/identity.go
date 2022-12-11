eb474102831d3a45a155fcd775504106f88dde00
Sean Kelly <keenskelly@gmail.com>
2022-12-10 23:09:39 -0800

    WIP - Need to work on IdentityIfc to get it working

--- lib/Auth/identity.go
+++ lib/Auth/identity.go
@@ -4,15 +4,15 @@ package auth
 This is the identity of an authenticated "user" (maybe it's not even a human?). We will provide some convenience mechanisms for association between this Identity and ScopedAuthorizationGrants.
 */
 
-type AuthenticatedIdentityIfc interface {
+type IdentityIfc interface {
 }
 
-type authenticatedIdentity struct {
+type identity struct {
 }
 
 // Factory Functions
 
-func NewAuthenticatedIdentity() *authenticatedIdentity {
-	return &authenticatedIdentity{ }
+func NewIdentity() *identity {
+	return &identity{ }
 }
 
